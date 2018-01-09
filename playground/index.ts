46ab81c7e190af3fe697059ff33d672d8344783f
maxisam <maxisam@gmail.com>
2018-01-09 17:27:52 -0600

    chore: update demo

--- playground/index.ts
+++ playground/index.ts
@@ -4,23 +4,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { NgxTrimModule } from '../dist/index';
+import { NgxTrimModule } from 'ngx-trim';
 
 
 @Component({
-  selector: 'app',
+  selector: 'my-app',
+  styles:[`
+      :host{
+      display: flex;
+      top:0;
+      bottom: 0;
+      left: 0;
+      right: 0;
+      position: absolute;
+      align-items: center;
+      justify-content: space-around;
+    }
+  `],
   template: `<div>
-    <input type="text"  placeholder="input" [(ngModel)]="testInput" ngxTrim>
-    <div>{{testInput}}</div>
-  </div>`
+  <label>Trim after focus out</label>
+	<input type="text" class="form-control" placeholder="Type something" [(ngModel)]="testInput" ngxTrim>
+  <div>{{testInput}}</div>
+  <br/>
+  <label>Trim after key up</label>
+	<input type="text" class="form-control" placeholder="Type something" [(ngModel)]="testInput2" ngxTrim='keyup'>
+	<div>{{testInput2}}</div>
+</div>`
 })
 class AppComponent { }
 
 @NgModule({
   bootstrap: [AppComponent],
   declarations: [AppComponent],
-  imports: [BrowserModule, NgxTrimModule]
+  imports: [BrowserModule, FormsModule, NgxTrimModule ]
 })
 class AppModule { }
 
