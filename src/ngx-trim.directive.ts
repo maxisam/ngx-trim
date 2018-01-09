82047f75e7288cc42659007a25a2e3d9b8fe3e7a
maxisam <maxisam@gmail.com>
2018-01-09 17:26:11 -0600

    feat: handle different events to trim

--- src/ngx-trim.directive.ts
+++ src/ngx-trim.directive.ts
@@ -1,5 +1,6 @@
-import { Directive, ElementRef, forwardRef, Renderer2, HostListener } from '@angular/core';
-import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
+import { Directive, ElementRef, forwardRef, HostListener, OnInit, Renderer2 } from '@angular/core';
+import { Input } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 const TRIM_VALUE_ACCESSOR = {
   provide: NG_VALUE_ACCESSOR,
@@ -12,7 +13,8 @@ const TRIM_VALUE_ACCESSOR = {
   selector: 'input[ngxTrim]',
   providers: [TRIM_VALUE_ACCESSOR]
 })
-export class NgxTrimDirective implements ControlValueAccessor {
+export class NgxTrimDirective implements ControlValueAccessor, OnInit {
+  @Input('ngxTrim') onEvent: 'keyup' | 'focusout';
   _onChange(_: any) { }
   _onTouched() { }
   registerOnChange(fn: (value: any) => any): void { this._onChange = fn; }
@@ -25,11 +27,18 @@ export class NgxTrimDirective implements ControlValueAccessor {
   writeValue(value: any): void {
     value && this._renderer.setProperty(this._elementRef.nativeElement, 'value', value);
   }
+  ngOnInit() {
+    this.onEvent = this.onEvent || 'focusout';
+  }
 
-  @HostListener('keyup', ['$event.target'])
-  _onKeyUp(element: HTMLInputElement) {
-    let val = element.value.trim();
-    this.writeValue(val);
-    this._onChange(val);
+  @HostListener('keyup', ['$event'])
+  @HostListener('focusout', ['$event'])
+  _onKeyUp(event: Event) {
+    if (this.onEvent === event.type) {
+      let element = <HTMLInputElement>event.target;
+      let val = element.value.trim();
+      this.writeValue(val);
+      this._onChange(val);
+    }
   }
 }
